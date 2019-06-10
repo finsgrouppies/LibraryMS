9653cc0628a33bf69187e2b1239d59282397b168
finsgrouppies <finsgrouppies@gmail.com>
2019-06-10 16:28:28 +0300

    user control

--- LibraryMS/User.Designer.cs
+++ LibraryMS/User.Designer.cs
@@ -29,43 +29,98 @@
         private void InitializeComponent()
         {
             this.label2 = new System.Windows.Forms.Label();
-            this.panel_addbook = new System.Windows.Forms.Panel();
-            this.butt_addbook = new System.Windows.Forms.Button();
+            this.panel_adduser = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.butt_adduser = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.text_addbook = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.panel_addbook.SuspendLayout();
+            this.butt_addusermain = new System.Windows.Forms.Button();
+            this.panel_deleteuser = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.butt_deleteuser = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.butt_deleteusermain = new System.Windows.Forms.Button();
+            this.panel_adduser.SuspendLayout();
+            this.panel_deleteuser.SuspendLayout();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(337, 15);
+            this.label2.Location = new System.Drawing.Point(387, 18);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 24);
             this.label2.TabIndex = 4;
             this.label2.Text = "Users";
             // 
-            // panel_addbook
+            // panel_adduser
             // 
-            this.panel_addbook.Controls.Add(this.butt_addbook);
-            this.panel_addbook.Controls.Add(this.label1);
-            this.panel_addbook.Controls.Add(this.text_addbook);
-            this.panel_addbook.Location = new System.Drawing.Point(43, 123);
-            this.panel_addbook.Name = "panel_addbook";
-            this.panel_addbook.Size = new System.Drawing.Size(344, 303);
-            this.panel_addbook.TabIndex = 6;
+            this.panel_adduser.Controls.Add(this.label4);
+            this.panel_adduser.Controls.Add(this.textBox2);
+            this.panel_adduser.Controls.Add(this.label3);
+            this.panel_adduser.Controls.Add(this.textBox1);
+            this.panel_adduser.Controls.Add(this.butt_adduser);
+            this.panel_adduser.Controls.Add(this.label1);
+            this.panel_adduser.Controls.Add(this.text_addbook);
+            this.panel_adduser.Location = new System.Drawing.Point(43, 123);
+            this.panel_adduser.Name = "panel_adduser";
+            this.panel_adduser.Size = new System.Drawing.Size(344, 303);
+            this.panel_adduser.TabIndex = 6;
             // 
-            // butt_addbook
+            // label4
             // 
-            this.butt_addbook.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butt_addbook.Location = new System.Drawing.Point(110, 136);
-            this.butt_addbook.Name = "butt_addbook";
-            this.butt_addbook.Size = new System.Drawing.Size(168, 34);
-            this.butt_addbook.TabIndex = 6;
-            this.butt_addbook.Text = "Add Book";
-            this.butt_addbook.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Last Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(110, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 26);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "First Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(110, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 26);
+            this.textBox1.TabIndex = 7;
+            // 
+            // butt_adduser
+            // 
+            this.butt_adduser.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butt_adduser.Location = new System.Drawing.Point(110, 248);
+            this.butt_adduser.Name = "butt_adduser";
+            this.butt_adduser.Size = new System.Drawing.Size(168, 34);
+            this.butt_adduser.TabIndex = 6;
+            this.butt_adduser.Text = "Add";
+            this.butt_adduser.UseVisualStyleBackColor = true;
+            this.butt_adduser.Click += new System.EventHandler(this.butt_adduser_Click);
             // 
             // label1
             // 
@@ -73,9 +128,10 @@
             this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(3, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 18);
+            this.label1.Size = new System.Drawing.Size(60, 18);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Book Name";
+            this.label1.Text = "User ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // text_addbook
             // 
@@ -85,27 +141,123 @@
             this.text_addbook.Size = new System.Drawing.Size(168, 26);
             this.text_addbook.TabIndex = 4;
             // 
-            // button1
+            // butt_addusermain
             // 
-            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(67, 67);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 34);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Add Book";
-            this.button1.UseVisualStyleBackColor = true;
+            this.butt_addusermain.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butt_addusermain.Location = new System.Drawing.Point(43, 67);
+            this.butt_addusermain.Name = "butt_addusermain";
+            this.butt_addusermain.Size = new System.Drawing.Size(168, 34);
+            this.butt_addusermain.TabIndex = 7;
+            this.butt_addusermain.Text = "Add User";
+            this.butt_addusermain.UseVisualStyleBackColor = true;
+            this.butt_addusermain.Click += new System.EventHandler(this.butt_addusermain_Click);
+            // 
+            // panel_deleteuser
+            // 
+            this.panel_deleteuser.Controls.Add(this.label5);
+            this.panel_deleteuser.Controls.Add(this.textBox3);
+            this.panel_deleteuser.Controls.Add(this.label6);
+            this.panel_deleteuser.Controls.Add(this.textBox4);
+            this.panel_deleteuser.Controls.Add(this.butt_deleteuser);
+            this.panel_deleteuser.Controls.Add(this.label7);
+            this.panel_deleteuser.Controls.Add(this.textBox5);
+            this.panel_deleteuser.Location = new System.Drawing.Point(468, 123);
+            this.panel_deleteuser.Name = "panel_deleteuser";
+            this.panel_deleteuser.Size = new System.Drawing.Size(344, 303);
+            this.panel_deleteuser.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Last Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(110, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(168, 26);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 18);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "First Name";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(110, 57);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(168, 26);
+            this.textBox4.TabIndex = 7;
+            // 
+            // butt_deleteuser
+            // 
+            this.butt_deleteuser.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butt_deleteuser.Location = new System.Drawing.Point(110, 248);
+            this.butt_deleteuser.Name = "butt_deleteuser";
+            this.butt_deleteuser.Size = new System.Drawing.Size(168, 34);
+            this.butt_deleteuser.TabIndex = 6;
+            this.butt_deleteuser.Text = "Delete";
+            this.butt_deleteuser.UseVisualStyleBackColor = true;
+            this.butt_deleteuser.Click += new System.EventHandler(this.butt_deleteuser_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 18);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "User ID";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(110, 23);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(168, 26);
+            this.textBox5.TabIndex = 4;
+            // 
+            // butt_deleteusermain
+            // 
+            this.butt_deleteusermain.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butt_deleteusermain.Location = new System.Drawing.Point(644, 67);
+            this.butt_deleteusermain.Name = "butt_deleteusermain";
+            this.butt_deleteusermain.Size = new System.Drawing.Size(168, 34);
+            this.butt_deleteusermain.TabIndex = 12;
+            this.butt_deleteusermain.Text = "Delete User";
+            this.butt_deleteusermain.UseVisualStyleBackColor = true;
+            this.butt_deleteusermain.Click += new System.EventHandler(this.butt_deleteusermain_Click);
             // 
             // User
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel_addbook);
+            this.Controls.Add(this.butt_deleteusermain);
+            this.Controls.Add(this.panel_deleteuser);
+            this.Controls.Add(this.butt_addusermain);
+            this.Controls.Add(this.panel_adduser);
             this.Controls.Add(this.label2);
             this.Name = "User";
             this.Size = new System.Drawing.Size(851, 501);
-            this.panel_addbook.ResumeLayout(false);
-            this.panel_addbook.PerformLayout();
+            this.Load += new System.EventHandler(this.User_Load);
+            this.panel_adduser.ResumeLayout(false);
+            this.panel_adduser.PerformLayout();
+            this.panel_deleteuser.ResumeLayout(false);
+            this.panel_deleteuser.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,10 +266,23 @@
         #endregion
 
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel_addbook;
-        private System.Windows.Forms.Button butt_addbook;
+        private System.Windows.Forms.Panel panel_adduser;
+        private System.Windows.Forms.Button butt_adduser;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox text_addbook;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button butt_addusermain;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel_deleteuser;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button butt_deleteuser;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button butt_deleteusermain;
     }
 }
--- LibraryMS/User.cs
+++ LibraryMS/User.cs
@@ -16,5 +16,42 @@ namespace LibraryMS
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void HideUser()
+        {
+            panel_adduser.Enabled = false;
+            panel_deleteuser.Enabled = false;
+
+        }
+        private void User_Load(object sender, EventArgs e)
+        {
+            HideUser();
+        }
+
+        private void butt_adduser_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void butt_deleteuser_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void butt_addusermain_Click(object sender, EventArgs e)
+        {
+            HideUser();
+            panel_adduser.Enabled = true;
+        }
+
+        private void butt_deleteusermain_Click(object sender, EventArgs e)
+        {
+            HideUser();
+            panel_deleteuser.Enabled = true;
+        }
     }
 }
